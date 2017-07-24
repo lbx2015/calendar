40c90202fd9cb30c7d71f766e10b48f157121d3f
RIKING\zw.zhang <zw.zhang@riking.net>
2017-07-24 17:25:04 +0800

    MOD uddate the style of creating reminder

--- app/src/main/java/com/riking/calendar/fragment/CreateReminderFragment.java
+++ app/src/main/java/com/riking/calendar/fragment/CreateReminderFragment.java
@@ -1,11 +1,13 @@
 package com.riking.calendar.fragment;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 
 import com.riking.calendar.R;
 
@@ -19,6 +21,9 @@ public class CreateReminderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.create_reminder_fragment, container, false);
+        Switch s = (Switch) v.findViewById(R.id.simpleSwitch);
+//        s.getThumbDrawable().setColorFilter(getContext().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
+//        s.getTrackDrawable().setColorFilter(getContext().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
         return v;
     }
 
