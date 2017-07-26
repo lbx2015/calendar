6a7d072ff69d8c78694a1dfe0b5b2ca6583ea6f7
RIKING\zw.zhang <zw.zhang@riking.net>
2017-07-26 16:19:42 +0800

    MOD get density

--- app/src/main/java/com/riking/calendar/util/ZR.java
+++ app/src/main/java/com/riking/calendar/util/ZR.java
@@ -1,6 +1,8 @@
 package com.riking.calendar.util;
 
+import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 
 /**
  * Created by zw.zhang on 2017/7/14.
@@ -27,5 +29,32 @@ public class ZR {
      */
     public static float convertPxToDp(Context context, float px) {
         return px / context.getResources().getDisplayMetrics().density;
+    }
+
+    public void getDensity(Activity activity) {
+        float density = activity.getResources().getDisplayMetrics().density;
+        if (density > 4.0f) {
+            //it is a xxxhdpi display
+            //load a extra extra extra high resolution image (600x600px, for example)
+        } else if (density > 3.0f) {
+            Log.d("zzw", "it is a xxhdpi display");
+            //it is a xxhdpi display
+            //load a extra extra high resolution image (450x450px, for example)
+        } else if (density > 2.0f) {
+            Log.d("zzw", "it is a xhdpi display");
+            //it is a xhdpi display
+            //load a extra high resolution image (300x300px, for example)
+        } else if (density > 1.5f) {
+            Log.d("zzw", "it is a hdpi display");
+            //it is a hdpi display
+            //load a high resolution image (200x200px, for example)
+        } else if (density > 1.0f) {
+            //it is a mdpi display
+            //load a medium resolution image (150x150px, for example)
+        } else if (density > 0.75f) {
+            //it is a ldpi display
+            //load a low resolution image (100x100px, for example)
+        }
+
     }
 }
