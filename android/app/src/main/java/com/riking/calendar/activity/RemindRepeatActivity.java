669d6d2e7d567c2d6136111dcf1e1617de950a98
RIKING\zw.zhang <zw.zhang@riking.net>
2017-07-28 14:02:38 +0800

    MOD adding back listener

--- android/app/src/main/java/com/riking/calendar/activity/RemindRepeatActivity.java
+++ android/app/src/main/java/com/riking/calendar/activity/RemindRepeatActivity.java
@@ -3,7 +3,7 @@ package com.riking.calendar.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
 
 import com.riking.calendar.R;
 
@@ -11,10 +11,22 @@ import com.riking.calendar.R;
  * Created by zw.zhang on 2017/7/24.
  */
 
-public class RemindRepeatActivity extends AppCompatActivity {
+public class RemindRepeatActivity extends AppCompatActivity implements View.OnClickListener {
+    private View backButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remind_repeat);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.back: {
+                onBackPressed();
+                break;
+            }
+        }
     }
 }
--- android/app/src/main/java/com/riking/calendar/activity/RemindWayActivity.java
+++ android/app/src/main/java/com/riking/calendar/activity/RemindWayActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.riking.calendar.R;
 import com.riking.calendar.widget.wheelpicker.core.AbstractWheelDecor;
@@ -17,25 +18,26 @@ import com.riking.calendar.widget.wheelpicker.widget.curved.WheelMinutePicker;
  * Created by zw.zhang on 2017/7/24.
  */
 
-public class RemindWayActivity extends AppCompatActivity {
+public class RemindWayActivity extends AppCompatActivity implements View.OnClickListener{
     WheelMinutePicker wmp;
-
+    private View backButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        Log.d("zzw", this + "on create");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remind_kind);
         wmp = (WheelMinutePicker) findViewById(R.id.minute_picker);
-        /*wmp.setWheelDecor(true, new AbstractWheelDecor() {
-            @Override
-            public void drawDecor(Canvas canvas, Rect rectLast, Rect rectNext, Paint paint) {
-                paint.setColor(Color.BLACK);
-                paint.setTextAlign(Paint.Align.CENTER);
-                paint.setTextSize(getResources().getDimensionPixelSize(R.dimen.normal_font_size_18));
-                canvas.drawText("分", rectNext.centerX(),
-                        rectNext.centerY() - (paint.ascent() + paint.descent()) / 2.0F, paint);
-                canvas.drawText("提前", rectLast.centerX(), rectLast.centerY() - (paint.ascent() + paint.descent()) / 2.0F, paint);
+        backButton = findViewById(R.id.back);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.back:{
+                onBackPressed();
+                break;
             }
-        });*/
+        }
+
     }
 }
