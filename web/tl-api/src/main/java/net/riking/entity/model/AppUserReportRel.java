87e364be93481b2cadf4fa3458a0d606352f7255
lucky.liu <lucky.liu@RIKINGSHT085.RIKING.NET>
2017-07-28 17:28:27 +0800

    桥表修改

--- web/tl-api/src/main/java/net/riking/entity/model/AppUserReportRel.java
+++ web/tl-api/src/main/java/net/riking/entity/model/AppUserReportRel.java
@@ -32,20 +32,9 @@ public class AppUserReportRel extends BaseEntity {
 	// 报表ID
 	@Column(name = "report_Id", length = 32)
 	private String reportId;
-	// 启用状态
-	@Column(name = "enabled", length = 2)
-	private String enabled;
-	// 删除状态    0删除 1显示
+	// 删除状态 0删除 1显示
 	@Column(name = "delete_state", length = 2)
 	private String deleteState;
-	// 生效日期
-	@Temporal(TemporalType.DATE)
-	@Column(name = "startTime")
-	private Date startTime;
-	// 失效日期
-	@Temporal(TemporalType.DATE)
-	@Column(name = "overTime")
-	private Date overTime;
 
 	public String getId() {
 		return id;
@@ -79,34 +68,10 @@ public class AppUserReportRel extends BaseEntity {
 		this.reportId = reportId;
 	}
 
-	public String getEnabled() {
-		return enabled;
-	}
-
-	public void setEnabled(String enabled) {
-		this.enabled = enabled;
-	}
-
-	public Date getStartTime() {
-		return startTime;
-	}
-
-	public void setStartTime(Date startTime) {
-		this.startTime = startTime;
-	}
-
-	public Date getOverTime() {
-		return overTime;
-	}
-
-	public void setOverTime(Date overTime) {
-		this.overTime = overTime;
-	}
-
 	@Override
 	public String toString() {
-		return "AppUserReport [id=" + id + ", appUserId=" + appUserId + ", reportId=" + reportId + ", enabled="
-				+ enabled + ", startTime=" + startTime + ", overTime=" + overTime + "]";
+		return "AppUserReportRel [id=" + id + ", appUserId=" + appUserId + ", reportId=" + reportId + ", deleteState="
+				+ deleteState + "]";
 	}
 
 }
