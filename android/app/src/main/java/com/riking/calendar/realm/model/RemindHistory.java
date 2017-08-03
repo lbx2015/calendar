3ff68fa80031401e64646b9c75456a67f9657fd3
RIKING\zw.zhang <zw.zhang@riking.net>
2017-08-03 17:56:20 +0800

    MOD create remindhistory table and mapping to json format

---
+++ android/app/src/main/java/com/riking/calendar/realm/model/RemindHistory.java
@@ -0,0 +1,31 @@
+package com.riking.calendar.realm.model;
+
+import android.support.annotation.NonNull;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.UUID;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by zw.zhang on 2017/7/12.
+ */
+
+public class RemindHistory extends RealmObject {
+    @PrimaryKey
+    @SerializedName("todo_id")
+    public String id = UUID.randomUUID().toString();
+    public String user_id;
+    //The title of the reminder
+    @NonNull
+    @SerializedName("content")
+    public String title;
+    //the day of the reminder(yyyyMMdd)
+    @SerializedName("star_date")
+    public String day;
+    //the time of the reminder(HHmm)
+    @SerializedName("start_time")
+    public String time;
+}
