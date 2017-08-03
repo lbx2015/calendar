9c6d99090b0b4ca4e245686e85275ac5968b9f0d
RIKING\zw.zhang <zw.zhang@riking.net>
2017-08-03 18:41:52 +0800

    MOD update the time format

--- android/app/src/main/java/com/riking/calendar/adapter/ReminderAdapter.java
+++ android/app/src/main/java/com/riking/calendar/adapter/ReminderAdapter.java
@@ -2,7 +2,6 @@ package com.riking.calendar.adapter;
 
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
-import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +12,6 @@ import com.riking.calendar.R;
 import com.riking.calendar.realm.model.Reminder;
 import com.riking.calendar.util.DateUtil;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -39,12 +37,13 @@ public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.MyView
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
         Reminder r = reminders.get(position);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm:ss");
-        holder.time.setText(simpleDateFormat.format(r.time));
+        String HH = r.time.substring(2);
+        String mm = r.time.substring(0, 2);
+        holder.time.setText(HH + ":" + mm);
         holder.title.setText(r.title);
 
         //set a different color for the future reminders.
-        if (DateUtil.get(r.day,r.time).compareTo(new Date()) > 0) {
+        if (DateUtil.get(r.day, r.time).compareTo(new Date()) > 0) {
             holder.time.setTextColor(ContextCompat.getColor(holder.time.getContext(), R.color.color_29a1f7));
             holder.title.setTextColor(ContextCompat.getColor(holder.title.getContext(), R.color.color_323232));
         }
