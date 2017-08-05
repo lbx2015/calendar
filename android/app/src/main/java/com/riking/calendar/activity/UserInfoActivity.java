cd88acea38d8066fd0993914b5ee6891e86c765c
RIKING\zw.zhang <zw.zhang@riking.net>
2017-08-05 11:38:42 +0800

    MOD adding back click listener

--- android/app/src/main/java/com/riking/calendar/activity/UserInfoActivity.java
+++ android/app/src/main/java/com/riking/calendar/activity/UserInfoActivity.java
@@ -3,6 +3,7 @@ package com.riking.calendar.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.riking.calendar.R;
 
@@ -10,10 +11,20 @@ import com.riking.calendar.R;
  * Created by zw.zhang on 2017/8/5.
  */
 
-public class UserInfoActivity extends AppCompatActivity {
+public class UserInfoActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        findViewById(R.id.back).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.back: {
+                onBackPressed();
+            }
+        }
     }
 }
