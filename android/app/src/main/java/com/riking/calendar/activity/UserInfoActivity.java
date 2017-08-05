449fe9f381d0603b9d89d3d0ddfe3747321b59eb
RIKING\zw.zhang <zw.zhang@riking.net>
2017-08-05 11:34:43 +0800

    MOD create personal info sub Activity

---
+++ android/app/src/main/java/com/riking/calendar/activity/UserInfoActivity.java
@@ -0,0 +1,19 @@
+package com.riking.calendar.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.riking.calendar.R;
+
+/**
+ * Created by zw.zhang on 2017/8/5.
+ */
+
+public class UserInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}
