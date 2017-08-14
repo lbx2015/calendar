a34e2357976c92705c0fae9def965c7262d83ecd
RIKING\zw.zhang <zw.zhang@riking.net>
2017-08-14 15:04:18 +0800

    MOD add the login activity

--- android/app/src/main/java/com/riking/calendar/activity/LoginActivity.java
+++ android/app/src/main/java/com/riking/calendar/activity/LoginActivity.java
@@ -3,6 +3,7 @@ package com.riking.calendar.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.riking.calendar.R;
 
@@ -15,5 +16,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
