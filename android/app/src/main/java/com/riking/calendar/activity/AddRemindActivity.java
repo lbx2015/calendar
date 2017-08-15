d754b78fbc71dcba024ec7801e7a5b00a641d2d6
RIKING\zw.zhang <zw.zhang@riking.net>
2017-08-15 16:15:31 +0800

    MOD adding local alarm notification

--- android/app/src/main/java/com/riking/calendar/activity/AddRemindActivity.java
+++ android/app/src/main/java/com/riking/calendar/activity/AddRemindActivity.java
@@ -1,5 +1,10 @@
 package com.riking.calendar.activity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -14,6 +19,7 @@ import android.widget.Toast;
 
 import com.flyco.tablayout.SlidingTabLayout;
 import com.google.gson.Gson;
+import com.ldf.calendar.Const;
 import com.riking.calendar.R;
 import com.riking.calendar.fragment.CreateReminderFragment;
 import com.riking.calendar.pojo.ReminderModel;
@@ -21,9 +27,12 @@ import com.riking.calendar.realm.model.Reminder;
 import com.riking.calendar.realm.model.Task;
 import com.riking.calendar.retrofit.APIClient;
 import com.riking.calendar.retrofit.APIInterface;
+import com.riking.calendar.service.ReminderService;
+import com.riking.calendar.util.DateUtil;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.UUID;
 
@@ -39,8 +48,10 @@ import retrofit2.Response;
 public class AddRemindActivity extends AppCompatActivity {
     MyPagerAdapter pagerAdapter;
     CreateReminderFragment reminderFragment;
+    SharedPreferences preference;
     CreateTaskFragment taskFragment;
     APIInterface apiInterface;
+    String userId;
     private ViewPager viewPager;
     private Realm realm;
 
@@ -56,6 +67,8 @@ public class AddRemindActivity extends AppCompatActivity {
         SlidingTabLayout topTabLayout = (SlidingTabLayout) findViewById(R.id.top_tab_layout);
         topTabLayout.setViewPager(viewPager);
         apiInterface = APIClient.getClient().create(APIInterface.class);
+        preference = getSharedPreferences(Const.PREFERENCE_FILE_NAME, MODE_PRIVATE);
+        userId = preference.getString(Const.USER_ID, null);
     }
 
     public void onClickCancel(View v) {
@@ -65,15 +78,9 @@ public class AddRemindActivity extends AppCompatActivity {
     public void onClickConfirm(View v) {
         // Create the Realm instance
         realm = Realm.getDefaultInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+        final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
 
         final String id = sdf.format(new Date());
-
-        final String reminderTitle = reminderFragment.remindTitle.getText().toString();
-        if (reminderTitle == null || reminderTitle.trim().equals("")) {
-            Toast.makeText(this, "提醒内容不能为空", Toast.LENGTH_SHORT).show();
-            return;
-        }
 
         //insert  to realm
         // All writes must be wrapped in a transaction to facilitate safe multi threading
@@ -82,6 +89,13 @@ public class AddRemindActivity extends AppCompatActivity {
             public void execute(Realm realm) {
                 //remind fragment
                 if (viewPager.getCurrentItem() == 0) {
+
+                    final String reminderTitle = reminderFragment.remindTitle.getText().toString();
+                    if (reminderTitle == null || reminderTitle.trim().equals("")) {
+                        Toast.makeText(AddRemindActivity.this, "提醒内容不能为空", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
                     // Add a remind
                     final Reminder reminder = realm.createObject(Reminder.class, id);
                     SimpleDateFormat dayFormat = new SimpleDateFormat("yyyyMMdd");
@@ -90,12 +104,49 @@ public class AddRemindActivity extends AppCompatActivity {
                     Date reminderDate = reminderFragment.time.getTime();
                     reminder.day = dayFormat.format(reminderDate);
                     reminder.time = timeFormat.format(reminderDate);
+                    Log.d("zzw", "reminder time" + reminder.time);
                     reminder.repeatFlag = reminderFragment.repeatFlag;
                     reminder.repeatWeek = reminderFragment.repeatWeek;
                     reminder.aheadTime = reminderFragment.aheadTime;
                     reminder.isRemind = reminderFragment.isRemind;
                     reminder.isAllDay = reminderFragment.isAllDay;
                     reminder.reminderTime = reminderDate;
+                    reminder.userId = userId;
+
+
+                    //set reminder
+                    Intent intent = new Intent(AddRemindActivity.this, ReminderService.class);
+                    intent.putExtra(Const.REMINDER_TITLE, reminder.title);
+                    PendingIntent pendingIntent = PendingIntent.getService(AddRemindActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+//                    alarmManager.cancel(pendingIntent);
+                    Calendar reminderCalendar = reminderFragment.time;
+                    reminderCalendar.set(java.util.Calendar.MINUTE, reminderFragment.time.get(java.util.Calendar.MINUTE) - reminder.aheadTime);
+                    if (reminder.isRemind == 1 && reminder.repeatFlag == 0) {
+                        alarmManager.setExact(AlarmManager.RTC_WAKEUP, reminderCalendar.getTimeInMillis(), pendingIntent);
+//                        long intervalMillis = 1000;
+//                        alarmManager.setWindow(AlarmManager.RTC_WAKEUP, reminderCalendar.getTimeInMillis(),
+//                                intervalMillis, pendingIntent);
+                    } else if (reminder.isRemind == 1 && reminder.repeatFlag == 3) {
+                        String repeatWeek = reminder.repeatWeek;
+                        long intervalMillis = 0;
+                        long remindTime;
+                        //repeat each day
+                        if (repeatWeek.length() == 7) {
+                            intervalMillis = 24 * 3600 * 1000;
+                            remindTime = DateUtil.getRepeatReminderTime(0, reminderCalendar.getTimeInMillis());
+                            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, remindTime, intervalMillis, pendingIntent);
+                        } else {
+                            //repeat the alarm on each week days selected.
+                            intervalMillis = 24 * 3600 * 1000 * 7;
+                            for (int i = 1; i <= 7; i++) {
+                                if (repeatWeek.contains(String.valueOf(i))) {
+                                    remindTime = DateUtil.getRepeatReminderTime(i, reminderCalendar.getTimeInMillis());
+                                    alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, remindTime, intervalMillis, pendingIntent);
+                                }
+                            }
+                        }
+                    }
                 }
                 //task fragment
                 else {
@@ -107,58 +158,61 @@ public class AddRemindActivity extends AppCompatActivity {
                         task.remindTime = taskFragment.calendar.getTime();
                     }
                     task.title = taskFragment.title.getText().toString();
+                    task.user_id = userId;
                 }
             }
         });
-        //remind fragment
-        if (viewPager.getCurrentItem() == 0) {
-            Reminder r = realm.where(Reminder.class).equalTo("id", id).findFirst();
-            ReminderModel mode = new ReminderModel();
-            mode.id = r.id;
-            mode.aheadTime = r.aheadTime;
-            mode.clientType = r.clientType;
-            mode.currentWeek = r.currentWeek;
-            mode.day = r.day;
-            mode.isAllDay = r.isAllDay;
-            mode.isRemind = r.isRemind;
-            mode.repeatFlag = r.repeatFlag;
-            mode.time = r.time;
-            mode.title = r.title;
-            mode.deleteState = r.deleteState;
-            mode.endTime = r.endTime;
-            mode.repeatWeek = r.repeatWeek;
-            Log.d("zzw", "reminder model : " + mode);
+        if (preference.getBoolean(Const.IS_LOGIN, false)) {
+            //remind fragment
+            if (viewPager.getCurrentItem() == 0) {
+                Reminder r = realm.where(Reminder.class).equalTo("id", id).findFirst();
+                ReminderModel mode = new ReminderModel();
+                mode.id = r.id;
+                mode.aheadTime = r.aheadTime;
+                mode.clientType = r.clientType;
+                mode.currentWeek = r.currentWeek;
+                mode.day = r.day;
+                mode.isAllDay = r.isAllDay;
+                mode.isRemind = r.isRemind;
+                mode.repeatFlag = r.repeatFlag;
+                mode.time = r.time;
+                mode.title = r.title;
+                mode.deleteState = r.deleteState;
+                mode.endTime = r.endTime;
+                mode.repeatWeek = r.repeatWeek;
+                mode.userId = r.userId;
+                Log.d("zzw", "reminder model : " + mode);
 
-            Call<ResponseBody> call = apiInterface.createRemind(mode);
-            call.enqueue(new retrofit2.Callback<ResponseBody>() {
-                @Override
-                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                    ResponseBody r = response.body();
-                    try {
-                        if (r == null) {
-                            Toast.makeText(getBaseContext(), getBaseContext().getString(R.string.create_failed), Toast.LENGTH_SHORT).show();
+                Call<ResponseBody> call = apiInterface.createRemind(mode);
+                call.enqueue(new retrofit2.Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        ResponseBody r = response.body();
+                        try {
+                            if (r == null) {
+                                Toast.makeText(getBaseContext(), getBaseContext().getString(R.string.create_failed), Toast.LENGTH_SHORT).show();
+                            }
+                            String s = r.source().readUtf8();
+                            String s2 = s.replace("\\", "");
+                            int i = s2.indexOf("}");
+                            int l = s2.lastIndexOf("}");
+                            Gson gson = new Gson();
+                            ReminderModel m;
+                            m = gson.fromJson(s2.substring(10, i + 1), ReminderModel.class);
+                            Log.d("zzw", response.code() + " index of " + i + "" + l + "success " + m);
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
-                        String s = r.source().readUtf8();
-                        String s2 = s.replace("\\", "");
-                        int i = s2.indexOf("}");
-                        int l = s2.lastIndexOf("}");
-                        Gson gson = new Gson();
-                        ReminderModel m;
-                        m = gson.fromJson(s2.substring(10, i + 1), ReminderModel.class);
-                        Log.d("zzw", response.code() + " index of " + i + "" + l + "success " + m);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+
                     }
 
-                }
-
-                @Override
-                public void onFailure(Call<ResponseBody> call, Throwable t) {
-                    Log.d("zzw", "fail" + t.getMessage());
-                }
-            });
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Log.d("zzw", "fail" + t.getMessage());
+                    }
+                });
+            }
         }
-
         onBackPressed();
     }
 
--- android/app/src/main/java/com/riking/calendar/realm/model/Reminder.java
+++ android/app/src/main/java/com/riking/calendar/realm/model/Reminder.java
@@ -27,7 +27,7 @@ public class Reminder extends RealmObject {
     public int endTime;
     //1...7
     public String repeatWeek;
-    //0,1,2,3
+    //0(not repeat),1,2,3(week repeat)
     public byte repeatFlag;
     //0-6
     public byte currentWeek;
