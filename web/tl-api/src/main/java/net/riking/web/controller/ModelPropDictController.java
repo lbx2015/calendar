cda4c86f2678e500d2ad43f188fd8245b09118be
you.fei <you.fei@RIKINGSHT096.RIKING.NET>
2017-08-21 17:27:55 +0800

    代码优化 报文详情h5页面修改 各国节假日各国图面名称修改

--- web/tl-api/src/main/java/net/riking/web/controller/ModelPropDictController.java
+++ web/tl-api/src/main/java/net/riking/web/controller/ModelPropDictController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
