5ce605a79ad0aba5f30caa749e88adf55a95e27a
you.fei <you.fei@RIKINGSHT096.RIKING.NET>
2017-08-22 10:51:18 +0800

    手机端h5页面加载数据问题

--- web/tl-api/src/main/java/net/riking/config/CodeDef.java
+++ web/tl-api/src/main/java/net/riking/config/CodeDef.java
@@ -7,5 +7,11 @@ public class CodeDef {
 	
 	public static final class EMP{
 		public static final short NAME_IS_NULL = 1001;
+		//通用错误
+		public static final short GENERAL_ERR = -999;
+		//用户密码错误
+		public static final short USER_PASS_ERR = -998;
+		//登录超时
+		public static final short LOGIN_TIME_OUT = -997;
 	}
 }
