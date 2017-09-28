0324dc79d75fe7bab9a7d1a63bf37e1225d820e5
RIKING\zw.zhang <zw.zhang@riking.net>
2017-09-28 16:53:00 +0800

    MOD update the tabs using android's default TabLayouts

---
+++ android/app/src/main/java/com/riking/calendar/fragment/FinanceNewsFragment.java
@@ -0,0 +1,44 @@
+package com.riking.calendar.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ldf.calendar.Const;
+import com.ldf.calendar.Utils;
+import com.riking.calendar.R;
+import com.riking.calendar.activity.RemindHistoryActivity;
+import com.riking.calendar.activity.ViewPagerActivity;
+import com.riking.calendar.adapter.ReminderAdapter;
+import com.riking.calendar.realm.model.Reminder;
+import com.riking.calendar.retrofit.APIClient;
+import com.riking.calendar.util.CONST;
+import com.riking.calendar.util.Preference;
+import com.riking.calendar.view.CustomLinearLayout;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
+import io.realm.Sort;
+
+/**
+ * Created by zw.zhang on 2017/7/17.
+ */
+
+public class FinanceNewsFragment extends Fragment {
+}
--- android/app/src/main/java/com/riking/calendar/fragment/HomeFragment.java
+++ android/app/src/main/java/com/riking/calendar/fragment/HomeFragment.java
@@ -2,7 +2,12 @@ package com.riking.calendar.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +20,8 @@ import com.riking.calendar.R;
 
 public class HomeFragment extends Fragment {
     View v;
+    private ViewPager mViewPager;
+    private MyPagerAdapter mAdapter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,7 +33,65 @@ public class HomeFragment extends Fragment {
         if (v != null) {
             return v;
         }
+        v = inflater.inflate(R.layout.home_fragment, container, false);
+        mViewPager = (ViewPager) v.findViewById(R.id.pager);
+        mAdapter = new MyPagerAdapter(getChildFragmentManager());
+        mViewPager.setAdapter(mAdapter);//给ViewPager设置适配器
+        TabLayout tabLayout = v.findViewById(R.id.top_tab_layout);
+        tabLayout.setupWithViewPager(mViewPager);
 
         return v;
     }
+
+
+    //ViewPager适配器
+    class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 2;//页卡数
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Log.d("zzw", "getItem: " + position);
+            switch (position) {
+                case 0:
+                    return new TopicFragment();
+                case 1:
+                    return new FinanceNewsFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            Log.d("zzw", "instantiateItem: " + position);
+
+            return super.instantiateItem(container, position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView(container);//删除页卡
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "话题动态";
+                case 1:
+                    return "行业资讯";
+            }
+
+            return null;
+        }
+
+    }
+
 }
