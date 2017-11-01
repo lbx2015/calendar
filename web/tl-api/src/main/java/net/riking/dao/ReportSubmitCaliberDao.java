815dd6cf2bacc30d3c6e0b792c5a56b023062940
yuantao <tao.yuan@riking.net>
2017-11-01 13:40:16 +0800

    edit

--- web/tl-api/src/main/java/net/riking/dao/ReportSubmitCaliberDao.java
+++ web/tl-api/src/main/java/net/riking/dao/ReportSubmitCaliberDao.java
@@ -6,7 +6,7 @@ import net.riking.entity.model.QueryReport;
 
 public interface ReportSubmitCaliberDao {
 	Set<QueryReport> findAllByFreeDatefromReportId(String userId,Integer week,Integer ten,Integer month,Integer season,Integer halfYear,Integer Year,Integer isWorkDay);
-	Set<QueryReport> findAllfromReportId(); 
+	Set<QueryReport> findAllfromReportId();
 	int  updateDelayDateAfter(String type,String remarks);
 	int  updateDelayDateBefer(String type,String remarks,String frequency);
 }
