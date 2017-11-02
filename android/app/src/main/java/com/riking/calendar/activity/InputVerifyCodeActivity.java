0abae1990474742f92d7cd3b0ab96c08ef554f56
RIKING\zw.zhang <zw.zhang@riking.net>
2017-11-02 18:24:54 +0800

    MOD adding input verifycode

--- android/app/src/main/java/com/riking/calendar/activity/InputVerifyCodeActivity.java
+++ android/app/src/main/java/com/riking/calendar/activity/InputVerifyCodeActivity.java
@@ -5,10 +5,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.necer.ncalendar.view.IdentifyingCodeView;
 import com.riking.calendar.R;
 import com.riking.calendar.util.StatusBarUtil;
 
@@ -16,12 +18,13 @@ import com.riking.calendar.util.StatusBarUtil;
  * Created by zw.zhang on 2017/8/14.
  */
 
-public class InputCellPhoneNumberActivity extends AppCompatActivity {
+public class InputVerifyCodeActivity extends AppCompatActivity {
+    private IdentifyingCodeView icv;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_input_phone_number);
+        setContentView(R.layout.activity_login_input_phone_verify_code);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow(); // in Activity's onCreate() for instance
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
@@ -32,13 +35,26 @@ public class InputCellPhoneNumberActivity extends AppCompatActivity {
         findViewById(R.id.enter_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(InputCellPhoneNumberActivity.this, InputVerifyCodeActivity.class));
+                startActivity(new Intent(InputVerifyCodeActivity.this, LoginActivity.class));
+            }
+        });
+
+        icv = (IdentifyingCodeView) findViewById(R.id.icv);
+
+        icv.setInputCompleteListener(new IdentifyingCodeView.InputCompleteListener() {
+            @Override
+            public void inputComplete() {
+//                startActivity(new Intent(InputVerifyCodeActivity.this, LoginActivity.class));
+            }
+
+            @Override
+            public void deleteContent() {
+                Log.i("icv_delete", icv.getTextContent());
             }
         });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    public void onClick(View view) {
+        icv.clearAllText();
     }
 }
