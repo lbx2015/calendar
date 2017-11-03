8a9ab237bf8e089ed2eba4d1f90b464f74415acc
RIKING\zw.zhang <zw.zhang@riking.net>
2017-11-03 16:23:53 +0800

    MOD adding video and gif support

--- android/app/src/main/java/com/riking/calendar/activity/LoginNavigateActivity.java
+++ android/app/src/main/java/com/riking/calendar/activity/LoginNavigateActivity.java
@@ -1,15 +1,14 @@
 package com.riking.calendar.activity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.AttributeSet;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.TextView;
 
@@ -34,17 +33,19 @@ public class LoginNavigateActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_navigation);
+        ButterKnife.bind(this);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow(); // in Activity's onCreate() for instance
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
 
         StatusBarUtil.setTranslucent(this);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
     }
 
     @Override
     protected void onStart() {
-        ButterKnife.bind(this);
         super.onStart();
         loginWithPhone.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,9 +53,53 @@ public class LoginNavigateActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginNavigateActivity.this, InputCellPhoneNumberActivity.class));
             }
         });
-        String gifPath = "http://g.hiphotos.baidu.com/image/h%3D300/sign=8e17c4cca6c3793162688029dbc6b784/a1ec08fa513d2697af090e975cfbb2fb4216d826.jpg";
+
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setBuiltInZoomControls(true);// support zoom
+        webSettings.setUseWideViewPort(true);// 这个很关键
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setSupportZoom(true);
+        //
+        webSettings.setDisplayZoomControls(false);
+//        String gifPath = "http://pic48.nipic.com/file/20140912/7487939_223919315000_2.jpg";
 //        String gifPath = "http://img.zcool.cn/community/018bcf575ed5200000018c1b264854.gif";
-//        String gifPath = "file:///android_asset/timg.gif";
-        webView.loadUrl(gifPath);
+
+        String gifPath = "file:///android_asset/kingfisher.jpg";
+        String videoPath = "file:///android_asset/LaunchTour.mp4";
+
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+
+        String data = "<HTML><div align=\"center\" margin=\"0px\"><IMG  width=\"100%\" height=\"100%\" src=\"" + gifPath + "\" margin=\"0px\"/></Div>";//设置图片位于webview的中间位置
+
+//        webView.loadDataWithBaseURL(gifPath, data, "text/html", "utf-8", null);
+
+        String viedo = "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "    <body style='margin:0'>\n" +
+                "        <video width=\"100%\" height=\"100%\" >\n" +
+                "              <source src=\"" + videoPath + "\" type=\"video/mp4\">\n" +
+                "        </video>\n" +
+                "    </body>\n" +
+                "</html>";
+
+        webView.loadDataWithBaseURL(gifPath, viedo, "text/html", "utf-8", null);
+        // 开启支持视频
+        webSettings.setPluginState(WebSettings.PluginState.ON);
+        webSettings.setGeolocationEnabled(true);
+        webView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                String js = "javascript: var v=document.getElementsByTagName('video')[0]; "
+                        + "v.play(); ";
+
+                webView.loadUrl(js);
+            }
+        }, 1000);
+        String jsFullScreen = "javascript: var v=document.getElementsByTagName('video')[0]; " + "v.webkitEnterFullscreen(); ";
+        webView.loadUrl(jsFullScreen);
+//        webView.loadUrl(gifPath);
     }
 }
