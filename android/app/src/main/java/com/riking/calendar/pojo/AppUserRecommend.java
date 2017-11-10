f5766a5a52bbd9478147920ffe3f51ecb96b2dc1
RIKING\zw.zhang <zw.zhang@riking.net>
2017-11-10 16:31:02 +0800

    MOD create user interesting reports bojo.

---
+++ android/app/src/main/java/com/riking/calendar/pojo/AppUserRecommend.java
@@ -0,0 +1,16 @@
+package com.riking.calendar.pojo;
+
+//@Entity
+//@Table(name = "t_app_user_recommend")
+public class AppUserRecommend {
+	//报表id
+//	@Id
+//	@Column(name = "report_id", length = 32)
+	public String reportId;
+	
+
+//	@Transient
+	public String reportName;
+
+
+}
---
+++ android/app/src/main/java/com/riking/calendar/pojo/AppUserReportResult.java
@@ -0,0 +1,28 @@
+package com.riking.calendar.pojo;
+
+import java.util.List;
+
+public class AppUserReportResult {
+	
+	private String userId;
+	
+	private List<String> list;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public List<String> getList() {
+		return list;
+	}
+
+	public void setList(List<String> list) {
+		this.list = list;
+	}
+
+
+}
