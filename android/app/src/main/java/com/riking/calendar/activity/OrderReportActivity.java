8743f2cb15457be8eb6a19486f22f08278f53fab
RIKING\zw.zhang <zw.zhang@riking.net>
2017-11-16 14:13:57 +0800

    MOD adding the scrolling function

--- android/app/src/main/java/com/riking/calendar/activity/OrderReportActivity.java
+++ android/app/src/main/java/com/riking/calendar/activity/OrderReportActivity.java
@@ -1,62 +1,39 @@
 package com.riking.calendar.activity;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.content.LocalBroadcastManager;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.hyphenate.EMClientListener;
-import com.hyphenate.EMMessageListener;
-import com.hyphenate.chat.EMClient;
-import com.hyphenate.chat.EMMessage;
-import com.hyphenate.chatuidemo.Constant;
-import com.hyphenate.chatuidemo.DemoHelper;
-import com.hyphenate.chatuidemo.db.InviteMessgeDao;
-import com.hyphenate.chatuidemo.ui.ContactListFragment;
-import com.hyphenate.chatuidemo.ui.ConversationListFragment;
-import com.hyphenate.chatuidemo.ui.GroupsActivity;
-import com.hyphenate.easeui.utils.EaseCommonUtils;
-import com.necer.ncalendar.utils.MyLog;
-import com.riking.calendar.BuildConfig;
 import com.riking.calendar.R;
-import com.riking.calendar.fragment.FourthFragment;
-import com.riking.calendar.fragment.HomeFragment;
-import com.riking.calendar.fragment.WorkFragment;
-import com.riking.calendar.jiguang.Logger;
-import com.riking.calendar.listener.CheckCallBack;
-import com.riking.calendar.pojo.AppVersionResult;
-import com.riking.calendar.retrofit.APIClient;
-import com.riking.calendar.util.AppInnerDownLoder;
-import com.riking.calendar.util.DownLoadApk;
-
-import java.util.List;
+import com.riking.calendar.view.InterestingReportLinearLayout;
+import com.riking.calendar.view.ZReportFlowLayout;
 
 /**
  * Created by zw.zhang on 2017/7/11.
  */
 
 public class OrderReportActivity extends AppCompatActivity {
+    ZReportFlowLayout zReportFlowLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_report);
+        zReportFlowLayout = findViewById(R.id.flow_layout);
+        /*//change the status bar color
+        getWindow().setStatusBarColor(getResources().getColor(R.color.white));
+        StatusBarUtil.setTranslucent(this);*/
+        for (int i = 0; i < 5; i++) {
+            //inflate the item view from layout xml
+            final InterestingReportLinearLayout root = (InterestingReportLinearLayout) LayoutInflater.from(this).inflate(R.layout.intersting_report_item, null);
+            //get view
+            final TextView reportNameTV = root.findViewById(R.id.interesting_report);
+            final ImageView checkImage = root.findViewById(R.id.check_image);
+            //set data
+            reportNameTV.setText("G01000");
+            zReportFlowLayout.addView(root);
+        }
     }
 }
