f01ab6eddd36e3d79a5eeb06fad40134988cc2e6
feiYouLian <1477099614@qq.com>
2017-11-23 10:29:47 +0800

    obj map 工具类

---
+++ web/tl-api/src/main/java/net/riking/util/Utils.java
@@ -0,0 +1,161 @@
+package net.riking.util;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import net.riking.entity.model.NewsInfo;
+
+public class Utils {
+	
+	/**
+	 * obj 转换成 Map
+	 * @used TODO
+	 * @param obj
+	 * @param deep
+	 * @return
+	 * @throws Exception
+	 */
+	public static <T extends Object> Map<String, Object> objProps2Map(T obj, boolean deep) {
+		Map<String,Object> map = new HashMap<>();
+		if(null==obj){
+			return map;
+		}
+		if(deep){
+			objPropsDeep2Map(obj, map);
+		}else{
+			objPropsEasy2Map(obj, map);
+		}
+		return map;
+	}
+	
+	/**
+	 * 深转换
+	 * @used TODO
+	 * @param obj
+	 * @param map
+	 */
+	private static <T extends Object> void objPropsDeep2Map(T obj, Map<String,Object> map){
+		try {
+			for (Class<? extends Object> clazz = obj.getClass(); clazz != null; clazz = clazz.getSuperclass()) {
+				Field[] fields = clazz.getDeclaredFields();
+				for (int i = 0; i < fields.length; i++) {
+					Field field = fields[i];
+					field.setAccessible(true);
+					String name = field.getName();
+					Object value = field.get(obj);
+					if(null!= value){
+						map.put(name, value);
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 浅转换
+	 * @used TODO
+	 * @param obj
+	 * @param map
+	 */
+	private static <T extends Object> void objPropsEasy2Map(T obj, Map<String,Object> map){
+		try {
+			Class<? extends Object> clazz = obj.getClass();
+			Field[] fields = clazz.getDeclaredFields();
+			for (int i = 0; i < fields.length; i++) {
+				Field field = fields[i];
+				field.setAccessible(true);
+				String name = field.getName();
+				Object value = field.get(obj);
+				if(null!= value){
+					map.put(name, value);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 *  map 转对象
+	 * @used TODO
+	 * @param map
+	 * @param clazz
+	 * @return
+	 */
+	public static <T extends Object> T map2Obj(Map<String,Object> map, Class<T> clazz){
+		T obj = null;
+		if(null == map || map.size()==0){
+			return obj;
+		}
+		try {
+			String id = clazz.getName().substring(clazz.getName().lastIndexOf(".")+1) + "Id";
+			Object removeValue = map.remove(id);
+			map.put("id", removeValue);
+			obj = clazz.newInstance();
+			List<Field> list = getAllFields(clazz);
+			Set<String> keySet = map.keySet();
+			for (String fieldName : keySet) {
+				for (Field field : list) {
+					if(field.getName().equals(fieldName)){
+						field.set(obj, map.get(fieldName));
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return obj;
+	}
+	
+	/**
+	 * 获取所有属性
+	 * @used TODO
+	 * @param clazz
+	 * @return
+	 */
+	private static  List<Field>  getAllFields(Class<?> clazz){
+		List<Field> list = new ArrayList<>();
+		try {
+			for ( ; clazz != null; clazz = clazz.getSuperclass()) {
+				Field[] fields = clazz.getDeclaredFields();
+				for (int i = 0; i < fields.length; i++) {
+					Field field = fields[i];
+					field.setAccessible(true);
+					list.add(field);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	
+	public static void main(String[] args) {
+		NewsInfo info = new NewsInfo();
+		
+		info.setAuthor("author");
+		info.setCreatedTime(new Date());
+		
+		Map<String, Object> map = objProps2Map(info, false);
+		
+		Set<String> keySet = map.keySet();
+		for (String key : keySet) {
+			System.err.println(key +"--"+ map.get(key));
+		}
+		
+		map.put("NewsInfoId", "12345");
+		
+		map.remove("serialVersionUID");
+		
+		NewsInfo info2 = map2Obj(map, NewsInfo.class);
+		
+		System.out.println(info2.getId()+ "--"+info2.getAuthor()+"--"+ info.getCreatedTime());
+	}
+}
