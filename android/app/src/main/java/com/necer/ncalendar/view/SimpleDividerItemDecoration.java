249ec9cca38fc7aed348184a2ec5b0e9d22b0112
RIKING\zw.zhang <zw.zhang@riking.net>
2017-11-24 19:30:47 +0800

    FIX the margin is not be consistent with set in xml. It caused by I adding a 6dp height  divider for the recyclerview, and don't adding space for it. It was solved by addig the 6 dp space

--- android/app/src/main/java/com/necer/ncalendar/view/SimpleDividerItemDecoration.java
+++ android/app/src/main/java/com/necer/ncalendar/view/SimpleDividerItemDecoration.java
@@ -2,11 +2,13 @@ package com.necer.ncalendar.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.riking.calendar.R;
+import com.riking.calendar.util.ZR;
 
 /**
  * Created by zw.zhang on 2017/11/23.
@@ -36,5 +38,12 @@ public class SimpleDividerItemDecoration extends RecyclerView.ItemDecoration {
             mDivider.setBounds(left, top, right, bottom);
             mDivider.draw(c);
         }
+    }
+
+    //adding space
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
+                               RecyclerView.State state) {
+        outRect.bottom = (int) ZR.convertDpToPx(view.getContext(),6);
     }
 }
