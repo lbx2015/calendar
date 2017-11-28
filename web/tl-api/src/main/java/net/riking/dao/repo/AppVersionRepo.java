8db15c357a7fab679e6be3a9c21c4c80091f101c
james <zm.you@riking.net>
2017-11-28 19:44:51 +0800

    提交APP版本接口

---
+++ web/tl-api/src/main/java/net/riking/dao/repo/AppVersionRepo.java
@@ -0,0 +1,37 @@
+package net.riking.dao.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import net.riking.entity.model.AppVersion;
+
+/**
+ * App版本
+ * 
+ * @author james.you
+ * @version crateTime：2017年11月28日 下午2:52:08
+ * @used TODO
+ */
+@Repository
+public interface AppVersionRepo extends JpaRepository<AppVersion, String>, JpaSpecificationExecutor<AppVersion> {
+
+	// TODO 暂时注释
+	// @Transactional
+	// @Modifying
+	// @Query(" update AppVersion set deleteState = '0' where id in ?1 ")
+	// int deleteById(Set<String> ids);
+
+	/***
+	 * 查询是否有App更新版本
+	 * @author james.you
+	 * @version crateTime：2017年11月28日 下午3:13:44
+	 * @used TODO
+	 * @param versionNo
+	 * @param clientType 1-IOS;2-Android
+	 * @return
+	 */
+	@Query("select new net.riking.entity.model.AppVersion(versionNo, enforce, url, remark) from AppVersion where is_deleted = 1 and versionNo > ?1 and client_type = ?2 and enabled=1 ")
+	AppVersion hasUpdateAppVersion(String versionNo, Integer clientType);
+}
