c95dec0a9b6468e791b82f02c1a4787aec7b54cb
zm.you@riking.net <zm.you@riking.net>
2017-12-06 10:10:16 +0800

    删除添加的enabled字段

--- web/tl-api/src/main/java/net/riking/entity/model/Report.java
+++ web/tl-api/src/main/java/net/riking/entity/model/Report.java
@@ -77,10 +77,6 @@ public class Report extends BasePageQueryProp {
 	@Column(name = "download_url", length = 128)
 	private String downloadUrl;
 	
-	@Comment("启用状态0-不启用；1-启用")
-	@Column(name = "enabled", length = 1)
-	private Integer enabled;
-
 	public Report(){}
 	
 
@@ -196,14 +192,4 @@ public class Report extends BasePageQueryProp {
 		this.downloadUrl = downloadUrl;
 	}
 
-
-	public Integer getEnabled() {
-		return enabled;
-	}
-
-
-	public void setEnabled(Integer enabled) {
-		this.enabled = enabled;
-	}
-	
 }
