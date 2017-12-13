6c95740ba4e6566a6dd8fb669229857ff5698488
zw.zhang <zw.zhang@riking.net>
2017-12-13 10:10:12 +0800

    Revert "MOD adding blank line"
    This reverts commit 2004a9a1fc991311634da60bff2a7d3620c5ee00.

--- android/app/src/main/java/com/riking/calendar/adapter/AnswerReplyListAdapter.java
+++ android/app/src/main/java/com/riking/calendar/adapter/AnswerReplyListAdapter.java
@@ -59,7 +59,6 @@ public class AnswerReplyListAdapter extends RecyclerView.Adapter<AnswerReplyList
         if (!TextUtils.isEmpty(to)) {
             toSpan = new SpannableString(to);
         }
-        
         SpannableString contentSpan = new SpannableString(content);
 
         ClickableSpan fromClick = new ClickableSpan() {
