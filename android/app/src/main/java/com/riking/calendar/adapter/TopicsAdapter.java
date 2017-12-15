6d7e7d1ccda5a9bd8533d3eec68a57a2aa28232c
zw.zhang <zw.zhang@riking.net>
2017-12-15 18:40:28 +0800

    MOD call the pick topic for question api

--- android/app/src/main/java/com/riking/calendar/adapter/TopicsAdapter.java
+++ android/app/src/main/java/com/riking/calendar/adapter/TopicsAdapter.java
@@ -1,28 +1,26 @@
 package com.riking.calendar.adapter;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.riking.calendar.R;
 import com.riking.calendar.activity.AddTopicActivity;
-import com.riking.calendar.view.CircleImageView;
+import com.riking.calendar.adapter.base.ZAdater;
+import com.riking.calendar.pojo.server.TopicResult;
+import com.riking.calendar.util.ZR;
+import com.riking.calendar.viewholder.SearchTopicViewHolder;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
-
-public class TopicsAdapter extends RecyclerView.Adapter<TopicsAdapter.MyViewHolder> {
+public class TopicsAdapter extends ZAdater<SearchTopicViewHolder> {
     private Context context;
-    private List<String> mList;
+    private List<TopicResult> mList;
 
     public TopicsAdapter(Context context) {
         this.context = context;
@@ -30,27 +28,29 @@ public class TopicsAdapter extends RecyclerView.Adapter<TopicsAdapter.MyViewHold
     }
 
     @Override
-    public TopicsAdapter.MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+    public SearchTopicViewHolder onCreateVH(ViewGroup viewGroup, int viewType) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(
-                R.layout.topic_item, viewGroup, false);
-        return new TopicsAdapter.MyViewHolder(view);
+                R.layout.search_topic_item, viewGroup, false);
+        return new SearchTopicViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(final TopicsAdapter.MyViewHolder h, int i) {
-        h.userName.setText("周润发" + i);
-        h.userName.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.com_icon_grade_v3, 0);
+    public void onBindVH(final SearchTopicViewHolder h, int i) {
+        TopicResult r = mList.get(i);
+        h.title.setText(r.title);
+        h.followButton.setVisibility(View.GONE);
+        h.summary.setText(ZR.getNumberString(r.followNum) + "人关注");
         RequestOptions options = new RequestOptions();
-        Glide.with(h.userImage.getContext()).load(R.drawable.img_user_head)
-                .apply(options.fitCenter())
-                .into(h.userImage);
+        Glide.with(h.topicImage.getContext()).load(r.topicUrl)
+                .apply(options.fitCenter().placeholder(R.drawable.banner))
+                .into(h.topicImage);
 
         h.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (context instanceof AddTopicActivity) {
                     AddTopicActivity a = (AddTopicActivity) context;
-                    a.clickAddTopic(h.userName.getText().toString());
+                    a.clickAddTopic(h.title.getText().toString());
                 }
             }
         });
@@ -58,34 +58,17 @@ public class TopicsAdapter extends RecyclerView.Adapter<TopicsAdapter.MyViewHold
 
 
     @Override
-    public int getItemCount() {
+    public int getCount() {
         return mList.size();
     }
 
-    public void add(String s) {
-        mList.add(s);
+    public void setData(List<TopicResult> s) {
+        mList = s;
         notifyDataSetChanged();
     }
 
     public void clear() {
         mList.clear();
         notifyDataSetChanged();
-    }
-
-
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-
-        @BindView(R.id.answer_summary)
-        public TextView answerSummary;
-        @BindView(R.id.user_name)
-        public TextView userName;
-        @BindView(R.id.user_icon)
-        public CircleImageView userImage;
-
-        public MyViewHolder(View itemView) {
-            super(itemView);
-            ButterKnife.bind(this, itemView);
-            itemView.setTag(this);
-        }
     }
 }
