2e72892e041dda5ac186365aaa2fa989a40cd787
zm.you@riking.net <zm.you@riking.net>
2017-12-21 16:58:43 +0800

    提交本地代码

--- web/tl-api/src/main/java/net/riking/service/impl/ReportServiceImpl.java
+++ web/tl-api/src/main/java/net/riking/service/impl/ReportServiceImpl.java
@@ -2,7 +2,9 @@ package net.riking.service.impl;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.transaction.Transactional;
 
